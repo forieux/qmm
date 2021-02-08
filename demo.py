315e3f3d5528b15b285da1a04aef123d7086d7a2
François Orieux <orieux@l2s.centralesupelec.fr>
2021-02-08 16:01:46 +0100

    Update figure

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 # pylint: disable=invalid-name
-#
+
+import time
 
 import numpy as np  # type: ignore
 import scipy.misc  # type: ignore
@@ -32,22 +33,32 @@ priorr_adeq = mmq.Criterion(diffr.forward, diffr.adjoint, pot, pot.gradient, hyp
 priorc_adeq = mmq.Criterion(diffc.forward, diffc.adjoint, pot, pot.gradient, hyper=0.01)
 
 # The Majorize-Minimize Quadratic Memory Gradient algorithm
+t0 = time.time()
 res, norm_grad = mmq.mmmg([data_adeq, priorr_adeq, priorc_adeq], init, max_iter=200)
+tot_time = time.time() - t0
 
 # Plot
 plt.figure(1)
 plt.clf()
 plt.subplot(2, 2, 1)
-plt.imshow(imag)
-# plt.axis([300, 500, 500, 300])
+plt.imshow(imag, vmin=imag.min(), vmax=imag.max())
+plt.axis([50, 250, 450, 250])
+plt.axis("off")
+plt.title("Original")
 plt.colorbar()
 plt.subplot(2, 2, 2)
-plt.imshow(data)
-# plt.axis([300, 500, 500, 300])
+plt.imshow(data, vmin=imag.min(), vmax=imag.max())
+plt.axis([50, 250, 450, 250])
+plt.axis("off")
+plt.title("Data")
 plt.colorbar()
 plt.subplot(2, 2, 3)
-plt.imshow(res)
-# plt.axis([300, 500, 500, 300])
+plt.imshow(res, vmin=imag.min(), vmax=imag.max())
+plt.axis([50, 250, 450, 250])
+plt.axis("off")
 plt.colorbar()
+plt.title("Restored")
 plt.subplot(2, 2, 4)
 plt.plot(norm_grad)
+plt.xlabel("Iteration")
+plt.title(f"Gradient norm (total time {tot_time:.2f} sec., {len(norm_grad)} it.)")
